dc81a197ca496a6b1e1a0966b4ef9ce6ab1db277
Faisal_khlid <Faisal.k.j@outlook.com>
2020-09-14 22:55:50 +0200

    قربت أخلص Table بالـلينير لاي اوت

--- app/src/main/java/ahmed/jaafuar/faisal/worlduniversities/invinity_table.kt
+++ app/src/main/java/ahmed/jaafuar/faisal/worlduniversities/invinity_table.kt
@@ -1,7 +1,8 @@
 package ahmed.jaafuar.faisal.worlduniversities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_invinity_table.*
 
 class invinity_table : AppCompatActivity() {
@@ -9,41 +10,52 @@ class invinity_table : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_invinity_table)
 
-        strBtn.setOnClickListener{
-            strBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
-            sunBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
-            monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
-        }//End srtBtn
 
-        sunBtn.setOnClickListener{
-            sunBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
-            strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
-            monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
-        }//End sunBtn
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // In landscape
+            strBtn.setOnClickListener{
+                strBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
+                sunBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
+                monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
+            }//End srtBtn
 
-        monBtn.setOnClickListener{
-            monBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
-            strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
-            sunBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
-        }//End monBtn
+            sunBtn.setOnClickListener{
+                sunBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
+                strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
+                monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
+            }//End sunBtn
 
-        thuBtn.setOnClickListener{
-            thuBtn.setBackgroundResource(R.color.samawy);    sunBtn.setBackgroundResource(R.color.transparent)
-            strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
-            monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
-        }//end thuBtn
+            monBtn.setOnClickListener{
+                monBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
+                strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
+                sunBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
+            }//End monBtn
 
-        wedBtn.setOnClickListener{
-            wedBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
-            strBtn.setBackgroundResource(R.color.transparent);sunBtn.setBackgroundResource(R.color.transparent)
-            monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
-        }//End wedBtn
+            thuBtn.setOnClickListener{
+                thuBtn.setBackgroundResource(R.color.samawy);    sunBtn.setBackgroundResource(R.color.transparent)
+                strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
+                monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
+            }//end thuBtn
 
-        tueBtn.setOnClickListener{
-            tueBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
-            strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
-            monBtn.setBackgroundResource(R.color.transparent); sunBtn.setBackgroundResource(R.color.transparent)
-        }//End tueBtn
+            wedBtn.setOnClickListener{
+                wedBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
+                strBtn.setBackgroundResource(R.color.transparent);sunBtn.setBackgroundResource(R.color.transparent)
+                monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
+            }//End wedBtn
+
+            tueBtn.setOnClickListener{
+                tueBtn.setBackgroundResource(R.color.samawy);    thuBtn.setBackgroundResource(R.color.transparent)
+                strBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
+                monBtn.setBackgroundResource(R.color.transparent); sunBtn.setBackgroundResource(R.color.transparent)
+            }//End tueBtn
+
+        }//End if
+        else {
+            // In portrait
+        }//End else
+
+
 
     }//End onCreate()
 
