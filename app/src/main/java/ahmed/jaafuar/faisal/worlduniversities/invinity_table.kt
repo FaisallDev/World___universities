0d752cbb78e5a2dd9d5e4580de61aeac2e015294
Faisal_khlid <Faisal.k.j@outlook.com>
2020-09-15 20:35:13 +0200

    ركبت اي دي للاشياء

--- app/src/main/java/ahmed/jaafuar/faisal/worlduniversities/invinity_table.kt
+++ app/src/main/java/ahmed/jaafuar/faisal/worlduniversities/invinity_table.kt
@@ -15,36 +15,51 @@ class invinity_table : AppCompatActivity() {
         if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             // In landscape
             satBtn.setOnClickListener{
+                // change background for days when click
                 satBtn.setBackgroundResource(R.color.days_clickable);    thuBtn.setBackgroundResource(R.color.transparent)
                 sunBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
                 monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
+
+                //change teacher,hall and subject names
+                firstSubjectName.setText(R.string.text_face_2); firstLectureHallName.setText(R.string.off)
+                firstLectureTeacherName.text = ""; timeFirstLecture.setText(R.string.empty)
+                secondLectureTeacherName.text = "" ; secondSubjectName.setText(R.string.text_face_2);
+                SecondLectureHallName.setText(R.string.off)
+                TimeSecondLectur.setText(R.string.empty); TimeSecondLectur.setText(R.string.empty)
+                TimeFourthLecture.setText(R.string.empty)
+
             }//End srtBtn
 
             sunBtn.setOnClickListener{
+                // change background for days when click
                 sunBtn.setBackgroundResource(R.color.days_clickable);    thuBtn.setBackgroundResource(R.color.transparent)
                 satBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
                 monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
             }//End sunBtn
 
             monBtn.setOnClickListener{
+                // change background for days when click
                 monBtn.setBackgroundResource(R.color.days_clickable);    thuBtn.setBackgroundResource(R.color.transparent)
                 satBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
                 sunBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
             }//End monBtn
 
             thuBtn.setOnClickListener{
+                // change background for days when click
                 thuBtn.setBackgroundResource(R.color.days_clickable);    sunBtn.setBackgroundResource(R.color.transparent)
                 satBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
                 monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
             }//end thuBtn
 
             wedBtn.setOnClickListener{
+                // change background for days when click
                 wedBtn.setBackgroundResource(R.color.days_clickable);    thuBtn.setBackgroundResource(R.color.transparent)
                 satBtn.setBackgroundResource(R.color.transparent);sunBtn.setBackgroundResource(R.color.transparent)
                 monBtn.setBackgroundResource(R.color.transparent); tueBtn.setBackgroundResource(R.color.transparent)
             }//End wedBtn
 
             tueBtn.setOnClickListener{
+                // change background for days when click
                 tueBtn.setBackgroundResource(R.color.days_clickable);    thuBtn.setBackgroundResource(R.color.transparent)
                 satBtn.setBackgroundResource(R.color.transparent);wedBtn.setBackgroundResource(R.color.transparent)
                 monBtn.setBackgroundResource(R.color.transparent); sunBtn.setBackgroundResource(R.color.transparent)
