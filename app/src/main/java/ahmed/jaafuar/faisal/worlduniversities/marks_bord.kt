4de9b304b4c08f62847fbcb4f5f7faf30ac7d59f
faisa <Faisal.k.j@outlook.com>
2020-09-25 01:56:05 +0300

    .

---
+++ app/src/main/java/ahmed/jaafuar/faisal/worlduniversities/marks_bord.kt
@@ -0,0 +1,15 @@
+package ahmed.jaafuar.faisal.worlduniversities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class marks_bord : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marks_bord)
+
+
+    }//End onCreate()
+
+
+}//End class
