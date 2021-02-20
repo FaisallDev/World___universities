05523a6170cf51cd808c4bd3613f8b4f4d1055f7
Faisal_khlid <Faisal.k.j@outlook.com>
2021-02-20 15:17:37 +0200

    Started new activity ( college border )

---
+++ app/src/main/java/ahmed/jaafuar/faisal/worlduniversities/college_border.kt
@@ -0,0 +1,13 @@
+package ahmed.jaafuar.faisal.worlduniversities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class college_border : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_college_border)
+
+    }//End onCreate()
+
+}//End Class
